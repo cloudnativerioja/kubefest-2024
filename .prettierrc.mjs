971341b64fb867786048bda5b5ba90533fb52f33
Adrian Barrio <statickidz@gmail.com>
2024-03-10 19:34:01 +0100

    feat: prettier format

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,13 +1,15 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [
-    'prettier-plugin-astro',
-  ],
+  plugins: ["prettier-plugin-astro"],
   overrides: [
     {
-      files: '*.astro',
+      files: "*.astro",
       options: {
-        parser: 'astro',
+        parser: "astro",
+        trailingComma: "none",
+        tabWidth: 2,
+        semi: true,
+        singleQuote: true,
       },
     },
   ],
