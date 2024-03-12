2d54816f0c9fcf9130b0fea6b696272096484a5b
Adrian Barrio <statickidz@gmail.com>
2024-03-12 14:51:11 +0100

    chore: update resource names

--- infra/main.tf
+++ infra/main.tf
@@ -5,14 +5,14 @@ resource "cloudflare_zone" "kubefest" {
   zone       = "kubefest.com"
 }
 
-resource "cloudflare_record" "kubefest" {
+resource "cloudflare_record" "kubefest_com" {
   zone    = "kubefest.com"
   name    = "kubefest.com"
   value   = "cloudnativerioja.github.io"
   type    = "CNAME"
 }
 
-resource "cloudflare_record" "kubefest" {
+resource "cloudflare_record" "www_kubefest_com" {
   zone    = "kubefest.com"
   name    = "www"
   value   = "cloudnativerioja.github.io"
