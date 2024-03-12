ff9c15f06a1ebf564c6f522eb69a5157782056ac
Adrian Barrio <statickidz@gmail.com>
2024-03-12 14:47:37 +0100

    feat: add cloudflare dns create

--- infra/main.tf
+++ infra/main.tf
@@ -4,3 +4,17 @@ resource "cloudflare_zone" "kubefest" {
   account_id = var.account_id
   zone       = "kubefest.com"
 }
+
+resource "cloudflare_record" "kubefest" {
+  zone    = "kubefest.com"
+  name    = "kubefest.com"
+  value   = "cloudnativerioja.github.io"
+  type    = "CNAME"
+}
+
+resource "cloudflare_record" "kubefest" {
+  zone    = "kubefest.com"
+  name    = "www"
+  value   = "cloudnativerioja.github.io"
+  type    = "CNAME"
+}
