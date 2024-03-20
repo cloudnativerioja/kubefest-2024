482a7d094133cce93f869d4c604de08445133d4f
RodrigoTomeES <rotome@unirioja.es>
2024-03-20 17:41:14 +0100

    perf: removed unused css and compress files in build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
+import purgecss from "astro-purgecss";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kubefest.com",
+  integrations: [purgecss(), compress()]
 });
